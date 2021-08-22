ff812ec52c3f53e36c149b6fcacb694a848b2fda
saiid20k <77093838+saiid20k@users.noreply.github.com>
2021-08-22 14:30:46 +0300

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,28 @@
+let toggleBtn = document.getElementById("toggleBtn");
+
+let links = document.getElementById("links");
+
+toggleBtn.onclick = function () {
+  
+    links.classList.toggle("menu");
+
+};
+
+document.addEventListener("click", (e) => {
+
+    if(e.target !== toggleBtn && e.target !== links){
+
+        if(links.classList.contains("menu")){
+
+            links.classList.toggle("menu");
+
+        }
+
+    } 
+});
+
+links.onclick = function (e) {
+
+    e.stopPropagation();
+
+}
